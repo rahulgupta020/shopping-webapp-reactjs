472cc8fbc9b0058c149930768eb3f9e967263241
Rahul Gupta <rahul@coalescence.tech>
2023-10-26 20:34:00 +0530

    Updates

---
+++ precache-manifest.741a1905f9e8194fddd26f50be831cd2.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f89b3a1f6650cef3a745d28394242b",
+    "url": "/shopping-webapp-reactjs/index.html"
+  },
+  {
+    "revision": "8151f0155c019cce7bab",
+    "url": "/shopping-webapp-reactjs/static/css/main.c2dd389d.chunk.css"
+  },
+  {
+    "revision": "7a08fff903bfdfa2cd76",
+    "url": "/shopping-webapp-reactjs/static/js/2.d845d76c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-webapp-reactjs/static/js/2.d845d76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8151f0155c019cce7bab",
+    "url": "/shopping-webapp-reactjs/static/js/main.3c46745e.chunk.js"
+  },
+  {
+    "revision": "7664a46dc880c04ee56e",
+    "url": "/shopping-webapp-reactjs/static/js/runtime-main.808904d7.js"
+  },
+  {
+    "revision": "bddfbc2c46efb0fea58f5b7862373d48",
+    "url": "/shopping-webapp-reactjs/static/media/accessoriesa.bddfbc2c.jpg"
+  },
+  {
+    "revision": "bfab46d4d0a1c66930ffac53e3c85c3e",
+    "url": "/shopping-webapp-reactjs/static/media/accessoriesb.bfab46d4.jpg"
+  },
+  {
+    "revision": "3cd9ccc0acc0d288cff0064d79a331a9",
+    "url": "/shopping-webapp-reactjs/static/media/accessoriesc.3cd9ccc0.jpg"
+  },
+  {
+    "revision": "84335cc32188ce8ca9e5fc6f35261cd2",
+    "url": "/shopping-webapp-reactjs/static/media/accessoriesd.84335cc3.jpg"
+  },
+  {
+    "revision": "fef9bde9a9de9a7113148d5e7ff1deef",
+    "url": "/shopping-webapp-reactjs/static/media/accessoriese.fef9bde9.jpg"
+  },
+  {
+    "revision": "12a2841dccfa2c9fff59f3df1d2a31be",
+    "url": "/shopping-webapp-reactjs/static/media/accessoriesf.12a2841d.jpg"
+  },
+  {
+    "revision": "224d6e26158d1e33e85eff10a88026f6",
+    "url": "/shopping-webapp-reactjs/static/media/clothesa.224d6e26.jpg"
+  },
+  {
+    "revision": "9b260782fd3645ff51bc2b36f9887375",
+    "url": "/shopping-webapp-reactjs/static/media/clothesb.9b260782.jpg"
+  },
+  {
+    "revision": "3e6b123f3c1fe72552c42364441b3bcb",
+    "url": "/shopping-webapp-reactjs/static/media/clothesc.3e6b123f.jpg"
+  },
+  {
+    "revision": "d27b62fa2c17c78a2c9537289e1edd92",
+    "url": "/shopping-webapp-reactjs/static/media/clothesd.d27b62fa.jpg"
+  },
+  {
+    "revision": "e97e81cec9f7dee205810b9ec1cd1f2e",
+    "url": "/shopping-webapp-reactjs/static/media/clothese.e97e81ce.jpg"
+  },
+  {
+    "revision": "16386a5b576e19773f56f381dbbdb9ef",
+    "url": "/shopping-webapp-reactjs/static/media/clothesf.16386a5b.jpg"
+  },
+  {
+    "revision": "d51f2735e3ac0c5c6c4de3c491cfd1c6",
+    "url": "/shopping-webapp-reactjs/static/media/grocerya.d51f2735.jpg"
+  },
+  {
+    "revision": "b9fd46a05afdf1f82026b6a137794e84",
+    "url": "/shopping-webapp-reactjs/static/media/groceryb.b9fd46a0.jpg"
+  },
+  {
+    "revision": "5fedce65e88842d4cabedaec012fc947",
+    "url": "/shopping-webapp-reactjs/static/media/groceryc.5fedce65.jpg"
+  },
+  {
+    "revision": "501f27e3b6765c656a6530d74040631d",
+    "url": "/shopping-webapp-reactjs/static/media/groceryd.501f27e3.jpg"
+  },
+  {
+    "revision": "41f904d14c3f405bc95390d6cf26ba77",
+    "url": "/shopping-webapp-reactjs/static/media/grocerye.41f904d1.jpg"
+  },
+  {
+    "revision": "eeea9e8621c6aeac51530de93f0f52ed",
+    "url": "/shopping-webapp-reactjs/static/media/groceryf.eeea9e86.jpg"
+  },
+  {
+    "revision": "47f276a48c7948c8a2aee05abc62f5ca",
+    "url": "/shopping-webapp-reactjs/static/media/homea.47f276a4.jpg"
+  },
+  {
+    "revision": "b5e5c5fd855232a572083fed405bf4ec",
+    "url": "/shopping-webapp-reactjs/static/media/homeb.b5e5c5fd.jpg"
+  },
+  {
+    "revision": "37b88bc7bace7e0573b1161fbbc74100",
+    "url": "/shopping-webapp-reactjs/static/media/homec.37b88bc7.jpg"
+  },
+  {
+    "revision": "c3dca024482eda0a8c1cb3b396b32936",
+    "url": "/shopping-webapp-reactjs/static/media/homed.c3dca024.jpg"
+  },
+  {
+    "revision": "a50703b087b4df14cdd5c06b42d909ca",
+    "url": "/shopping-webapp-reactjs/static/media/homee.a50703b0.jpg"
+  },
+  {
+    "revision": "b89fb7832a2b689480e1213f770703e6",
+    "url": "/shopping-webapp-reactjs/static/media/homef.b89fb783.jpg"
+  }
+]);
